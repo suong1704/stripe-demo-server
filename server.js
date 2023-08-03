5328cfb62cd2bf34fa2a41f8fd625797dd87f236
suongphan <ptthusuong@tma.com.vn>
2023-08-03 09:57:05 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+
+const cors = require("cors");
+
+// Sử dụng module cors
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+// Replace if using a different env file or config
+const env = require("dotenv").config({ path: "./.env" });
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+app.use(express.static(process.env.STATIC_DIR));
+
+app.get("/", (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(path);
+});
+
+app.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    // const { amount } = req.body;
+
+    // if (!amount) {
+    //   return res.status(400).send({
+    //     error: {
+    //       message: "Currency and amount are required.",
+    //     },
+    //   });
+    // }
+
+    const { amount } = req.body;
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
+app.listen(5000, () =>
+  console.log(`Node server listening at http://localhost:5000`)
+);
